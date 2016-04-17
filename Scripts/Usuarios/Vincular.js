541d6a51c066fd0656aebc83366c13bdcd4ea42a
juanxmx2 <juanma1993.2@gmail.com>
2016-04-16 21:02:12 -0500

    modificación de mensajes de alerta

--- Scripts/Usuarios/Vincular.js
+++ Scripts/Usuarios/Vincular.js
@@ -5,27 +5,27 @@ function vincular(){
     
     if($('#identificacion').val() === ""){
         $("#identificacion").focus();
-        alerts("La identificacion es obligatorio para la vinculacion");
+        alerts("La identificacion es obligatorio para la vinculacion" , "Formulario Incompleto");
     }
     else{
         validarSiNumero($("#identificacion").val());
         if(cent==0){
             if($('#telefono').val() === ""){
                 $("#telefono").focus();
-                alerts("El telefono es obligatorio para la vinculacion");
+                alerts("El telefono es obligatorio para la vinculacion", "Formulario Incompleto");
             }
             else{
                 validarSiNumero($("#telefono").val());
                 if(cent==0){
                     if($("#direccion").val() === ""){
                         $("#direccion").focus();
-                        alerts("La direccion es obligatoria para el registro");
+                        alerts("La direccion es obligatoria para el registro", "Formulario Incompleto");
                                 
                     }
                     else{
                         if($("#ciudad").val() === ""){
                             $("#ciudad").focus();
-                            alerts("La ciudad es obligatoria para el registro");
+                            alerts("La ciudad es obligatoria para el registro", "Formulario Incompleto");
                         }
                         else{
                                 document.getElementById("vinculacion").submit();
@@ -41,7 +41,7 @@ function vincular(){
 function validarSiNumero(numero){
     cent=0;
     if (!/^([0-9])*$/.test(numero)){
-        alerts("El valor " + numero + " no es un número");
+        alerts("Se requiere un dato numerico" , "Dato invalido");
         cent=1;
     } 
 }
